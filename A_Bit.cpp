bc77ffd74ca41f38828f0e9833b84d275d1519ed
Mosammat Fatematuz Johora <fatematuzj21@gmail.com>
2026-05-16 00:35:21 +0600

    Add files via upload

---
+++ A_Bit.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int x = 0;
+	int n;
+    cin>>n;
+    
+    while(n--){
+    string s;
+    cin>>s;
+
+    if(s[1]=='+'){
+       x++;
+    }else{
+       x--;
+    }
+}
+cout<< x;
+    
+
+    return 0;
+}
